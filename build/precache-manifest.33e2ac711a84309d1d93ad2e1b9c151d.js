f17bad78be04a7bab0670b288436e87f8a283299
shouryuha <shouryuha@gmail.com>
2021-04-14 23:43:32 -0300

    project

---
+++ build/precache-manifest.33e2ac711a84309d1d93ad2e1b9c151d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da3b55bc080e77ce4de1390cda85ec9",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "78808fd8a0f73b5da8e9",
+    "url": "/pokedex/static/css/main.fbc72e6f.chunk.css"
+  },
+  {
+    "revision": "5ffb4292bd25d8385164",
+    "url": "/pokedex/static/js/2.95609db1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.95609db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78808fd8a0f73b5da8e9",
+    "url": "/pokedex/static/js/main.882b687f.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
